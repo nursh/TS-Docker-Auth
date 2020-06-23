45960fccc4165cb41217e99c50fccd2a70aa8ec5
nursh <nsheikhmohammed01@gmail.com>
2020-06-23 00:07:20 -0400

    Add husky and lint-staged to config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
-
 const app = express();
 const port = 8080;
 
-app.listen(port, () => console.log(`App is running on port:${port}`))
+app.listen(port, () => console.log(`App is running on port:${port}`));
