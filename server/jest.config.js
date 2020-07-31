958210d711db62a2a1edcace3627ddd68f95a139
nursh <nsheikhmohammed01@gmail.com>
2020-07-30 21:45:06 -0400

    Add jest configuration for coverage.

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,9 +1,17 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
+const path = require('path');
 
 
 module.exports = {
   preset: '@shelf/jest-mongodb',
   transform: {
     ...tsjPreset.transform
-  }
+  },
+  coverageDirectory: path.join(__dirname, './coverage'),
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    '!**/src/types/**',
+    '!**/src/utils/**',
+    '!**/src/db/**'
+  ]
 };
