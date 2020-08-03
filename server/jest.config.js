1134db43ba5f1a082a0be497fd1a34f47a028a56
nursh <nsheikhmohammed01@gmail.com>
2020-08-02 20:32:41 -0400

    Add test results to codecov

--- server/jest.config.js
+++ server/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   transform: {
     ...tsjPreset.transform
   },
-  coverageDirectory: path.join(__dirname, '../coverage'),
+  coverageDirectory: path.join(__dirname, './coverage'),
   collectCoverageFrom: [
     '**/src/**/*.ts',
     '!**/src/types/**',
